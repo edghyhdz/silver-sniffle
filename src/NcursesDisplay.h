0d56c785545e05e9e8c9e54594602753132ff4e5
edghyhdz <edgar.hdz@hotmail.com>
2021-03-04 22:27:14 +0100

    Added DisplayMessages and TextBox methods

--- src/NcursesDisplay.h
+++ src/NcursesDisplay.h
@@ -2,13 +2,16 @@
 #ifndef NCURSES_DISPLAY_H
 #define NCURSES_DISPLAY_H
 
+#define FIELD_MAX_CHARS 32
+
+
 #include <curses.h>
+#include <mutex>
 
 namespace NcursesDisplay {
 void Display();
-
-void DisplayMessages(WINDOW *window);
-void SendText(WINDOW *window);
+void DisplayMessages(WINDOW *window, std::string number);
+void TextBox();
 
 } // namespace NcursesDisplay
 
