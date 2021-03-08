12788b3baf2e39a1a66904ad0430f0f3c400fd31
edghyhdz <edgar.hdz@hotmail.com>
2021-03-08 21:12:22 +0100

    Added auxiliary class ArrivingMessages, to deal with send/received messages from the tcp server

--- src/Client.h
+++ src/Client.h
@@ -1,35 +1,52 @@
-#ifndef CLIEN_H
-#define CLIEN_H
+#ifndef CLIENT_H
+#define CLIENT_H
 #include <string>
 #include <mutex>
 #include <curses.h>
 #include <vector>
 
+// Auxiliary class to queue messages in a thread-safe manner
+class ArrivingMessages {
+public:
+  // getters / setter
+  int getSize();
+  void pushBack(std::string response);
+  void setMessage(std::string message); 
+  std::vector<std::string> getResponses();
+  std::string getMessage(); 
+  bool messageIsEmpty(); 
+  void clearMessage(); 
+
+private:
+  std::vector<std::string> _responses;
+  std::string _message; 
+  std::mutex _mutex;
+};
 
 class Client {
 
 public:
-  Client(char *&ipAddress, char *&portNum, std::string *sendMsg, std::vector<std::string> *responses);
-  
-//   ~Client(); 
-//   Client(Client &&o) = default;
+  Client(char *&ipAddress, char *&portNum);
   int createConnection();
   void runClient();
   void runSendMessage(); 
 
-  void setResponse(std::string resonse); 
-  std::string getResponse(); 
+  // getters / setters related to arrivingMessages list
+  void setMessage(std::string message); 
+
+  std::vector<std::string> getResponses(); 
+  void clearMessage(); 
+  void pushBack(std::string message); 
+
 
 private:
   char *&_ipAddress;
   char *&_portNum;
   int _sockFD;
   int _connectR;
-  std::string * _sendMsg; 
-  std::string _response;
+  ArrivingMessages _arrivingMessages; 
   bool _updatedResponse;
   std::mutex *_mtx; 
-  std::vector<std::string> *_responses; 
 };
 
 #endif
