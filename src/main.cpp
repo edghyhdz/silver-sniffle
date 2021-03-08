3835349a74bcedcba10e16494136cfcc301db1be
edghyhdz <edgar.hdz@hotmail.com>
2021-03-08 08:15:38 +0100

    Added args to input local host for tcp server

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 /*
 g++ src/main.cpp src/NcursesDisplay.h src/NcursesDisplay.cpp src/utils/utility.h src/utils/trim.h -lcurses -lform -pthread
+g++ src/main.cpp src/NcursesDisplay.h src/NcursesDisplay.cpp src/utils/utility.h src/utils/trim.h src/utils/TimeStamp.h src/Client.h src/Client.cpp -lcurses -lform -pthread
 */
 
 #include "NcursesDisplay.h"
+#include "Client.h"
 
-int main() {
-    NcursesDisplay::Display(); 
+int main(int argc, char *argv[]) {
+
+    auto &ipAddress = argv[1];
+    auto &portNum   = argv[2];
+    NcursesDisplay::Display(ipAddress, portNum); 
 }
