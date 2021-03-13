13be5f23a8116593ac3ad5dd4a7c783fc930a100
edghyhdz <edgar.hdz@hotmail.com>
2021-03-13 19:24:31 +0100

    First commit RSA class

---
+++ src/RSA.h
@@ -0,0 +1,29 @@
+/*
+Reference: https://www.programmersought.com/article/37955188510/
+Reference certificates server
+https://stackoverflow.com/questions/11705815/client-and-server-communication-using-ssl-c-c-ssl-protocol-dont-works
+*/
+
+#ifndef RSA_H
+#define RSA_H
+
+#include "openssl/rsa.h"
+#include <string>
+
+/*
+Class definition
+*/
+
+class RSA {
+public:
+  void loadKeys();      // Load secret key
+  void encryptWithPK(); // Encrypt using public key
+  void encryptWithSK(); // Encrypt with secret key
+  void decryptWithSK(); // Decrypt with secret key
+  void decryptWithPK(); // Decrypt with public key
+private:
+  std::string _secretKey;
+  std::string _publicKey;
+};
+
+#endif
