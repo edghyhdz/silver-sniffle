d33921636deda41eab6f66c1d0ef833e47d1370d
edghyhdz <edgar.hdz@hotmail.com>
2021-03-14 07:23:30 +0100

    Declared loadKeys method to parse public and secret key to private class members

--- src/RSAEncrypt.cpp
+++ src/RSAEncrypt.cpp
@@ -8,14 +8,44 @@ https://stackoverflow.com/questions/11705815/client-and-server-communication-usi
 #include <openssl/pem.h>
 #include <stdio.h>
 #include "RSAEncrypt.h"
+#include <fstream> 
+#include <sstream>
 
 /*
 Class declaration
 */
 
-// As per default, it will load keys from
-// ./certificates folder
-void RSAEncrypt::loadKeys(){
+RSAEncrypt::RSAEncrypt(){
+    // Load keys to private members
+    this->loadKeys();
 
 }
 
+// As per default, it will load keys from
+// the ./certificates folder
+void RSAEncrypt::loadKeys(){
+
+    std::ifstream sKeyRaw("./certificates/private.pem"); 
+    std::string sKey;
+    if (sKeyRaw){
+        std::string line; 
+        while (getline(sKeyRaw, line)){
+            sKey += line + "\n"; 
+        }
+        // Remove trailing space
+        sKey = sKey.substr(0, sKey.size() - 1);
+        this->_secretKey = sKey; 
+    }
+
+    std::ifstream pKeyRaw("./certificates/public.pem"); 
+    std::string pKey;
+    if (pKeyRaw){
+        std::string line; 
+        while (getline(pKeyRaw, line)){
+            pKey += line + "\n"; 
+        }
+        // Remove trailing space
+        pKey = pKey.substr(0, pKey.size() - 1);
+        this->_publicKey = pKey; 
+    }
+}
