fbc6d631e3fd4bd2910279b995f612af0a15c0d0
edghyhdz <edgar.hdz@hotmail.com>
2021-03-16 22:05:03 +0100

    Added sendPublicKey, decryptMessage, and encryptMessage methods

--- src/Client.cpp
+++ src/Client.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <fstream>
 
 // Class definitions
 
@@ -26,17 +27,19 @@ void ArrivingMessages::pushBack(std::string response){
 }
 
 std::vector<std::string> ArrivingMessages::getResponses(){
-  std::lock_guard<std::mutex> lock(_mutex); 
-  return _responses; 
+  std::lock_guard<std::mutex> lock(_mutex);
+  return _responses;
 }
 
 void ArrivingMessages::setMessage(std::string message){
-  std::lock_guard<std::mutex> lock(_mutex); 
-  _message = message; 
+  std::lock_guard<std::mutex> lock(_mutex);
+  // Encrypt message with secret key
+  std::string encryptedMsg = this->rsa.encryptWithSK(Utils::trim(message), this->rsa.getSK()); 
+  _message = encryptedMsg;
 }
 
 std::string ArrivingMessages::getMessage(){
-  std::lock_guard<std::mutex> lock(_mutex); 
+  std::lock_guard<std::mutex> lock(_mutex);
   return _message; 
 }
 
@@ -131,6 +134,161 @@ void ArrivingMessages::updatePK(int user, std::string pK){
   Utils::updateDictionary(user, pK, &_userToPK); 
 }
 
+// Sends public key as first message
+std::string ArrivingMessages::sendPublicKey(){
+  // Load public key
+  // return it and send it
+  return this->rsa.getPK(); 
+}
+
+void ArrivingMessages::decryptMessage(int index, int user, std::string encryptedMsg){
+  std::lock_guard<std::mutex> lock(_mutex);
+  std::map<int, std::string>::iterator it = _userToPK.find(user);
+  std::string message = _responses[index];
+
+  // Decrypt message if found
+  if (it != _userToPK.end()) {
+    // Fetch public key
+    // std::string pK = it->second;
+
+    // Decrypt message with users public key
+    _responses[index] = "DECRYPTED: " + this->rsa.decryptWithPK(encryptedMsg, this->rsa.getPK());
+  }
+  // Else, indicate user is using external client
+  else {
+    _responses[index] = "~EXTERNAL~" + message;
+  }
+}
+
+void Client::decryptMessage(std::string message, int index){
+  bool notUser = Utils::findWord(message, "USER #");
+  std::string key, encryptedMsg, user_string;
+  int user;
+  char c; 
+
+  // If we have a 'USER' in the beginning of the message
+  if (!notUser) {
+    std::string message_copy = message; 
+    std::replace(message.begin(), message.end(), ' ', '_');
+    std::replace(message.begin(), message.end(), '#', ' ');
+    std::replace(message.begin(), message.end(), '_', ' ');
+
+    std::istringstream sline(message);
+    sline >> key >> user >> c;
+
+    user_string = "USER #" + std::to_string(user);
+
+    // if user number is followed by `:`
+    // it requires decryption
+    if (c == ':') {
+      size_t p = -1;
+      std::string tempWord = user_string + " ";
+      while ((p = message_copy.find(user_string)) != std::string::npos){
+        message_copy.replace(p, tempWord.length(), "");
+      } 
+      message_copy = message_copy.substr(1, message_copy.size());
+
+      // Decrypt received message
+      this->_arrivingMessages.decryptMessage(index, user, message_copy);
+    }
+  }
+}
+
+// This function will just be superficial
+// Will apend new users to be displayed in the users window
+int Client::addUser(std::string message){
+
+  bool hasNotLeft = Utils::findWord(message, "has joined the chat");
+
+  std::string key;
+  int value;
+
+  if (!hasNotLeft) {
+    std::replace(message.begin(), message.end(), ' ', '_');
+    std::replace(message.begin(), message.end(), '#', ' ');
+    std::replace(message.begin(), message.end(), '_', ' ');
+
+    std::istringstream sline(message);
+    sline >> key >> value;
+    return value; 
+  }
+  return -1; 
+}
+
+// Auxiliary method -> Add public key if found
+bool Client::addPK(std::string message){
+
+  bool notPK = Utils::findWord(message, "-----BEGIN RSA PUBLIC KEY-----");
+
+  std::string key;
+  int value; 
+  std::string pK;
+
+  if (!notPK) {
+    std::replace(message.begin(), message.end(), ' ', '$');
+    std::replace(message.begin(), message.end(), '\n', ';');
+    std::replace(message.begin(), message.end(), '#', ' ');
+
+    std::istringstream sline(message);
+    sline >> key >> value >> pK;
+
+    std::replace(pK.begin(), pK.end(), '$', ' ');
+    std::replace(pK.begin(), pK.end(), ';', '\n');
+    // Delete first two characters
+    pK = pK.substr(2, pK.size());
+    this->updatePK(value, pK); 
+    return true;
+  } 
+  return false; 
+}
+
+void Client::processMessages(){
+  // Check for new users or new users' public keys
+  // Decrypting happens also here, it is included in
+  std::vector<std::string> responses = this->getResponses(); 
+
+  if (_prevResponses.size() > 0) {
+    int diff = responses.size() - _prevResponses.size();
+    // If there are new messages
+    // Get the last <diff> items
+    if (diff > 0) {
+      for (int i = responses.size() - 1; i > _prevResponses.size() - 1; i--) {
+        // Check if this response is a new user or not
+        std::string tempResponse = responses[i];
+        int userID = addUser(tempResponse);
+
+        // check if it needs decryption
+        this->decryptMessage(tempResponse, i); 
+
+        // Add private key if any
+        bool addedKey = addPK(tempResponse); 
+        if (userID != -1) {
+          this->appendUser(userID); 
+        }
+      }
+      // In case first messages is the new user
+    } else if (responses.size() <= 2) {
+      for (int i = 0; i < responses.size(); i++) {
+        std::string tempResponse = responses[i];
+        int userID = addUser(tempResponse);
+
+        // check if it needs decryption
+        this->decryptMessage(tempResponse, i); 
+
+        // Add private key if any
+        bool addedKey = addPK(tempResponse);
+        if (userID != -1) {
+          this->appendUser(userID);
+        }
+      }
+    }
+  }
+
+  // Save snapshot of previous responses
+  _prevResponses = responses; 
+
+}
+
 void Client::updatePK(int user, std::string pK){
   this->_arrivingMessages.updatePK(user, pK); 
 }
@@ -161,7 +319,7 @@ std::vector<std::string> Client::getResponses(){
 
 Client::Client(char *&ipAddress, char *&portNum) : _ipAddress(ipAddress), _portNum(portNum){
   this->_mtx = new std::mutex();
-  this->createConnection(); 
+  this->createConnection();
 }
 
 Client::~Client(){
@@ -184,16 +342,30 @@ void Client::runClient(){
       // Get response into output string
       std::ostringstream ss;
       ss << buf;
+      std::string test; 
       // First message contains user information
       // Like user socket and user's rsa public key
       if (firstMessage) {
         this->_arrivingMessages.setUserData(ss.str());
         firstMessage = false;
-      } else { // All other messages
+      } else {
+        // All other messages
         // Check if message is related to user leaving chat
-        this->_arrivingMessages.removeUser(ss.str()); 
-        // Append message to chat vector
-        this->_arrivingMessages.pushBack(ss.str());
+        this->_arrivingMessages.removeUser(ss.str());
+
+        std::string temp_string = ss.str(); 
+        bool hasNotLeft = Utils::findWord(temp_string, "has joined the chat");
+
+        if (hasNotLeft) {
+          char *sendbuf = const_cast<char *>(buf);
+          for (int i = 0; i < 265; i++) {
+            test.push_back(sendbuf[i]);
+          }
+
+          this->_arrivingMessages.pushBack(test);
+        } else {
+          this->_arrivingMessages.pushBack(temp_string);
+        }
       }
     }
   } while (true);
@@ -207,7 +379,7 @@ void Client::runSendMessage(){
 
       // First message to server includes public key of client
       if (firstMessage){
-        std::string pK = this->sendPublicKey(); 
+        std::string pK = this->_arrivingMessages.sendPublicKey(); 
         send(_sockFD, pK.c_str(), pK.size() + 1, 0); 
         firstMessage = false; 
       }
@@ -215,9 +387,20 @@ void Client::runSendMessage(){
       // If there is a new message
       if (!this->_arrivingMessages.messageIsEmpty()){
         // Send to server
-        std::string tempMessage = this->_arrivingMessages.getMessage(); 
-        int sendRes = send(_sockFD, tempMessage.c_str(), tempMessage.size() + 1, 0);
+        std::string tempMessage = this->_arrivingMessages.getMessage();
+
+        // This is the only work around i found to go from string to char
+        // without loosing data due to encryption
+        // using c_str() did not work due to '\0' termination
+        char *sendbuf = new char[tempMessage.length()];
+        for(int i = 0; i < tempMessage.length(); i++)
+        {
+            sendbuf[i] = tempMessage[i];
+        }
+
+        int sendRes = send(_sockFD, sendbuf, tempMessage.size() + 1, 0);
         this->_arrivingMessages.clearMessage(); 
+        delete sendbuf; 
       }
     } while (true);
 }
@@ -261,11 +444,4 @@ int Client::createConnection() {
   }
 
   return 0; 
-}
-
-// Sends public key as first message
-std::string Client::sendPublicKey(){
-  // Load public key
-  // return it and send it
-  return "-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAu5XROlqBxbJoSaIYSRiPZ8jWvE5epjkmUoQFlE3B4CCewGZ4bqAe\n-----END RSA PUBLIC KEY-----"; 
 }
